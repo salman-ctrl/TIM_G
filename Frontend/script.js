92e25395be10f7e561bd1958ce8fd27e862dcf23
vani2522 <aryanirizkyrahmalia25@gmail.com>
2025-03-11 20:00:16 +0700

    add portofolio + asset

--- Frontend/script.js
+++ Frontend/script.js
@@ -11,3 +11,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
     elements.forEach((el) => observer.observe(el));
 });
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var slides = document.getElementsByClassName("portfolio-slide");
+    var dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].classList.add("active");
+}
+
+setInterval(() => { plusDivs(1); }, 3000);
