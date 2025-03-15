46b27ccae2fefe9c3a7f322eb68595151c05290f
aprizal11 <emiaprizal11gmail.com>
2025-03-16 03:36:54 +0700

    update testimonial

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,45 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const elements = document.querySelectorAll(".scroll-animation");
+  const elements = document.querySelectorAll(".scroll-animation");
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("show");
-            }
-        });
-    }, { threshold: 0.5 });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
 
-    elements.forEach((el) => observer.observe(el));
+  elements.forEach((el) => observer.observe(el));
 });
 
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-    showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function currentDiv(n) {
-    showDivs(slideIndex = n);
+  showDivs((slideIndex = n));
 }
 
 function showDivs(n) {
-    var slides = document.getElementsByClassName("portfolio-slide");
-    var dots = document.getElementsByClassName("dot");
-    
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+  var slides = document.getElementsByClassName("portfolio-slide");
+  var dots = document.getElementsByClassName("dot");
 
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
-    for (var i = 0; i < dots.length; i++) {
-        dots[i].classList.remove("active");
-    }
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-    slides[slideIndex - 1].style.display = "flex";
-    dots[slideIndex - 1].classList.add("active");
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].classList.add("active");
 }
 
-setInterval(() => { plusDivs(1); }, 3000);
+setInterval(() => {
+  plusDivs(1);
+}, 3000);
